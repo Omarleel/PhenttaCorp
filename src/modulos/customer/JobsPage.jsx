dc3551b00b35575e90a85d37c2e59729b34ae7a2
Cliver Omar Flores Pacheco <cliveromar1234@gmail.com>
2024-05-14 16:40:20 -0500

    Mejoras

--- src/modulos/customer/JobsPage.jsx
+++ src/modulos/customer/JobsPage.jsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import { CustomFormSlide } from "../../components/CustomFormSlide";
 import { JobsAvailable } from "./components/JobsAvailable";
 import { JobsForm } from "./components/JobsForm";
@@ -5,6 +6,12 @@ import { JobsPresentation } from "./components/JobsPresentation";
 
 export const JobsPage = () => {
     const steps = [<JobsPresentation />, <JobsAvailable />, /*<JobsForm />*/];
+    useEffect(() => {
+        window.scroll({
+        top: 0,
+        behavior: 'smooth'
+        });
+    }, [])
     return (
         <div className="container-page">
            <CustomFormSlide steps={steps} />
