0d5b530d40660c5e039c0b12730503e37bcc6b8d
Cliver Omar Flores Pacheco <cliveromar1234@gmail.com>
2024-05-13 19:01:13 -0500

    mejoras

--- src/modulos/customer/components/JobsAvailable.jsx
+++ src/modulos/customer/components/JobsAvailable.jsx
@@ -1,16 +1,17 @@
 export const JobsAvailable = () => {
     return (
         <div className="background-secondary flex max-md:flex-col items-center justify-center min-h-screen">
-            <div className="md:w-2/4 p-4 max-sm:hidden">
-                <img src="/assets/images/jobs/jobLess.svg" />
-            </div>
-            <div className="md:w-2/4 text-center mx-4 md:mx-8 my-8">
+
+            <div className="md:w-2/4 text-center mx-4 md:mx-8 my-8 md:order-2">
                 <h1>
                     Empleos en Phentta
                 </h1>
                 <h2>
                     Lamentablemente, en este momento no estamos contratando. ¡Gracias por tu interés en formar parte de nuestro equipo!
                 </h2>
+            </div>
+            <div className="w-3/12 p-4 md:order-1">
+                <img src="/assets/images/jobs/jobLess.svg" />
             </div>
 
         </div>
